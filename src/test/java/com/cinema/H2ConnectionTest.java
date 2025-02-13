c89fb1530eeda52e12ca81c111f20d86185fdb8c
rafael.boucher <simplicio.rsb@gmail.com>
2025-02-13 17:22:39 -0300

    chore: configura estrutura inicial do projeto com Maven, .gitignore, README.md e setup de testes com H2

---
+++ src/test/java/com/cinema/H2ConnectionTest.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class H2ConnectionTest {
+
+    @Test
+    public void contextLoads() {
+        // Se o contexto do Spring Boot carregar sem problemas, o teste passará.
+    }
+}
